e6e06bdd3194df403d8bc01b7da9280b695d8157
gkarkoski <guilherme.karkoski@gmail.com>
2024-05-05 00:22:38 -0300

    Entrega Projeto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
-console.log ("TESTE");
-console.log ("2");
+let NameHero;
+let Xp;
+let HeroLevel;
 
-let soma = 10+7;
-console.log (soma);
+//NameHero = prompt("Digite o nome do Herói.");
+//Xp = prompt("Digite a quantidade de XP.");
+
+//Xp = parseInt(Xp);
+
+
+NameHero = "Chico";
+Xp = 4236;
+
+
+if (Xp <= 1000) {
+    HeroLevel = "Ferro";
+}
+else if (Xp >= 1001 && Xp <= 2000) {
+    HeroLevel = "Bronze";
+}
+else if (Xp >= 2001 && Xp <= 5000) {
+    HeroLevel = "Prata";
+}
+else if (Xp >= 5001 && Xp <= 7000) {
+    HeroLevel = "Ouro";
+}
+else if (Xp >= 7001 && Xp <= 8000) {
+    HeroLevel = "Platina";
+}
+else if (Xp >= 8001 && Xp <= 9000) {
+    HeroLevel = "Ascendente";
+}
+else if (Xp >= 9001 && Xp <= 10000) {
+    HeroLevel = "Imortal";
+}
+else {
+    HeroLevel = "Radiante";
+}
+
+console.log(`O Herói de nome ${NameHero} está no nível de ${HeroLevel}`);
